d9982f21185387f22d5229c60d4b2194dcec099a
Jef LeCompte <jeffreylec@gmail.com>
2020-06-30 12:52:15 -0400

    feat: add strong/weak key report
    Resolves #10
    
    Signed-off-by: Jef LeCompte <jeffreylec@gmail.com>

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -20,55 +21,72 @@ type member struct {
 
 type keyTable struct {
 	keyDsaSize              uint32
-	keyEddsaSize            uint32
+	keyEcdsaSize            uint32
 	keyEd25519Size          uint32
 	keyRsaSize              uint32
+	keyStrongRsaSize        uint32
+	keyWeakRsaSize          uint32
 	userDsaSize             uint32
-	userEddsaSize           uint32
+	userEcdsaSize           uint32
 	userEd25519Size         uint32
 	userRsaSize             uint32
 	userWithKeySize         uint32
 	userWithoutKeySize      uint32
 	userWithMultipleKeySize uint32
-	totalKeySize            uint32
-	totalUserSize           uint32
-}
-
-func main() {
-	printReport(generateKeyTable(getKeys(getMembers())))
+	keySize                 uint32
+	strongKeySize           uint32
+	weakKeySize             uint32
+	userSize                uint32
+	userWithStrongKeySize   uint32
+	userWithWeakKeySize     uint32
+	userWithWeakKey         []member
 }
 
 func printReport(kt keyTable) {
 	withKey := [][]string{
 		{"users with keys",
 			"DSA",
-			fmt.Sprintf("%d (%.2f%%)", kt.keyDsaSize, float32(kt.keyDsaSize)/float32(kt.totalKeySize)*100),
-			fmt.Sprintf("%d (%.2f%%)", kt.userDsaSize, float32(kt.userDsaSize)/float32(kt.totalUserSize)*100)},
-		{"",
-			"RSA",
-			fmt.Sprintf("%d (%.2f%%)", kt.keyRsaSize, float32(kt.keyRsaSize)/float32(kt.totalKeySize)*100),
-			fmt.Sprintf("%d (%.2f%%)", kt.userRsaSize, float32(kt.userRsaSize)/float32(kt.totalUserSize)*100)},
+			fmt.Sprintf("%d (%.2f%%)", kt.keyDsaSize, float32(kt.keyDsaSize)/float32(kt.keySize)*100),
+			fmt.Sprintf("%d (%.2f%%)", kt.userDsaSize, float32(kt.userDsaSize)/float32(kt.userSize)*100)},
 		{"",
 			"ECDSA",
-			fmt.Sprintf("%d (%.2f%%)", kt.keyEddsaSize, float32(kt.keyEddsaSize)/float32(kt.totalKeySize)*100),
-			fmt.Sprintf("%d (%.2f%%)", kt.userEddsaSize, float32(kt.userEddsaSize)/float32(kt.totalUserSize)*100)},
+			fmt.Sprintf("%d (%.2f%%)", kt.keyEcdsaSize, float32(kt.keyEcdsaSize)/float32(kt.keySize)*100),
+			fmt.Sprintf("%d (%.2f%%)", kt.userEcdsaSize, float32(kt.userEcdsaSize)/float32(kt.userSize)*100)},
 		{"",
 			"Ed25519",
-			fmt.Sprintf("%d (%.2f%%)", kt.keyEd25519Size, float32(kt.keyEd25519Size)/float32(kt.totalKeySize)*100),
-			fmt.Sprintf("%d (%.2f%%)", kt.userEd25519Size, float32(kt.userEd25519Size)/float32(kt.totalUserSize)*100)},
+			fmt.Sprintf("%d (%.2f%%)", kt.keyEd25519Size, float32(kt.keyEd25519Size)/float32(kt.keySize)*100),
+			fmt.Sprintf("%d (%.2f%%)", kt.userEd25519Size, float32(kt.userEd25519Size)/float32(kt.userSize)*100)},
+		{"",
+			"RSA",
+			fmt.Sprintf("%d (%.2f%%)", kt.keyRsaSize, float32(kt.keyRsaSize)/float32(kt.keySize)*100),
+			fmt.Sprintf("%d (%.2f%%)", kt.userRsaSize, float32(kt.userRsaSize)/float32(kt.userSize)*100)},
 	}
 
 	withoutKey := [][]string{
 		{"users without keys",
 			"",
 			"",
-			fmt.Sprintf("%d (%.2f%%)", kt.userWithoutKeySize, float32(kt.userWithoutKeySize)/float32(kt.totalUserSize)*100)},
+			fmt.Sprintf("%d (%.2f%%)", kt.userWithoutKeySize, float32(kt.userWithoutKeySize)/float32(kt.userSize)*100)},
 	}
 
 	withMultipleKey := [][]string{{"users with multiple keys",
+		"",
+		"",
+		fmt.Sprintf("%d (%.2f%%)", kt.userWithMultipleKeySize, float32(kt.userWithMultipleKeySize)/float32(kt.userSize)*100)},
+	}
+
+	strongKey := [][]string{
+		{"users with strong keys",
 			"",
+			fmt.Sprintf("%d (%.2f%%)", kt.strongKeySize, float32(kt.strongKeySize)/float32(kt.keySize)*100),
+			fmt.Sprintf("%d (%.2f%%)", kt.userWithStrongKeySize, float32(kt.userWithStrongKeySize)/float32(kt.userWithKeySize)*100)},
+	}
+
+	weakKey := [][]string{
+		{"users with weak keys",
 			"",
-			fmt.Sprintf("%d (%.2f%%)", kt.userWithMultipleKeySize, float32(kt.userWithMultipleKeySize)/float32(kt.totalUserSize)*100)},
+			fmt.Sprintf("%d (%.2f%%)", kt.weakKeySize, float32(kt.weakKeySize)/float32(kt.keySize)*100),
+			fmt.Sprintf("%d (%.2f%%)", kt.userWithWeakKeySize, float32(kt.userWithWeakKeySize)/float32(kt.userWithKeySize)*100)},
 	}
 
 	t := tablewriter.NewWriter(os.Stdout)
@@ -77,7 +95,7 @@ func printReport(kt keyTable) {
 		tablewriter.Colors{tablewriter.FgCyanColor},
 		tablewriter.Colors{tablewriter.FgCyanColor},
 		tablewriter.Colors{tablewriter.FgCyanColor})
-	t.SetFooter([]string{"", "total", fmt.Sprintf("%d", kt.totalKeySize), fmt.Sprintf("%d", kt.totalUserSize)})
+	t.SetFooter([]string{"", "total", fmt.Sprintf("%d", kt.keySize), fmt.Sprintf("%d", kt.userSize)})
 	t.SetFooterColor(tablewriter.Colors{tablewriter.FgCyanColor},
 		tablewriter.Colors{tablewriter.FgCyanColor},
 		tablewriter.Colors{tablewriter.FgCyanColor},
@@ -86,14 +104,23 @@ func printReport(kt keyTable) {
 	t.AppendBulk(withKey)
 	t.AppendBulk(withoutKey)
 	t.AppendBulk(withMultipleKey)
+	t.AppendBulk(strongKey)
+	t.AppendBulk(weakKey)
 	t.Render()
+
+	if len(kt.userWithWeakKey) > 0 {
+		zap.S().Info("users with weak keys:")
+		for _, m := range kt.userWithWeakKey {
+			zap.S().Infof("%s", m.Login)
+		}
+	}
 }
 
 func generateKeyTable(ms []member) keyTable {
 	var wg sync.WaitGroup
 
 	var kt keyTable
-	kt.totalUserSize = uint32(len(ms))
+	kt.userSize = uint32(len(ms))
 
 	for _, m := range ms {
 		wg.Add(1)
@@ -103,40 +130,64 @@ func generateKeyTable(ms []member) keyTable {
 			defer wg.Done()
 
 			var (
-				hasDsa     bool
-				hasRsa     bool
-				hasEddsa   bool
-				hasEd25519 bool
+				hasDsa           bool
+				hasEcdsa         bool
+				hasEd25519       bool
+				hasRsa           bool
+				userHasStrongRsa bool
 			)
 
 			for _, key := range m.Keys {
-				atomic.AddUint32(&kt.totalKeySize, 1)
+				atomic.AddUint32(&kt.keySize, 1)
 
 				switch {
 				case strings.Contains(key, "ssh-dsa"):
-					atomic.AddUint32(&kt.keyDsaSize, 1)
 					hasDsa = true
-				case strings.Contains(key, "ssh-rsa"):
-					atomic.AddUint32(&kt.keyRsaSize, 1)
-					hasRsa = true
+					atomic.AddUint32(&kt.keyDsaSize, 1)
+					atomic.AddUint32(&kt.weakKeySize, 1)
 				case strings.Contains(key, "ssh-eddsa"):
-					atomic.AddUint32(&kt.keyEddsaSize, 1)
-					hasEddsa = true
+					hasEcdsa = true
+					atomic.AddUint32(&kt.keyEcdsaSize, 1)
+					atomic.AddUint32(&kt.weakKeySize, 1)
 				case strings.Contains(key, "ssh-ed25519"):
-					atomic.AddUint32(&kt.keyEd25519Size, 1)
 					hasEd25519 = true
+					atomic.AddUint32(&kt.keyEd25519Size, 1)
+					atomic.AddUint32(&kt.strongKeySize, 1)
+				case strings.Contains(key, "ssh-rsa"):
+					hasRsa = true
+					atomic.AddUint32(&kt.keyRsaSize, 1)
+					if isRsaStrong(key) {
+						userHasStrongRsa = true
+						atomic.AddUint32(&kt.keyStrongRsaSize, 1)
+						atomic.AddUint32(&kt.strongKeySize, 1)
+					} else {
+						userHasStrongRsa = false
+						atomic.AddUint32(&kt.keyWeakRsaSize, 1)
+						atomic.AddUint32(&kt.weakKeySize, 1)
+					}
 				}
 			}
 
 			switch {
 			case hasDsa:
 				atomic.AddUint32(&kt.userDsaSize, 1)
-			case hasRsa:
-				atomic.AddUint32(&kt.userRsaSize, 1)
-			case hasEddsa:
-				atomic.AddUint32(&kt.userEddsaSize, 1)
+				atomic.AddUint32(&kt.userWithWeakKeySize, 1)
+				kt.userWithWeakKey = append(kt.userWithWeakKey, m)
+			case hasEcdsa:
+				atomic.AddUint32(&kt.userEcdsaSize, 1)
+				atomic.AddUint32(&kt.userWithWeakKeySize, 1)
+				kt.userWithWeakKey = append(kt.userWithWeakKey, m)
 			case hasEd25519:
 				atomic.AddUint32(&kt.userEd25519Size, 1)
+				atomic.AddUint32(&kt.userWithStrongKeySize, 1)
+			case hasRsa:
+				atomic.AddUint32(&kt.userRsaSize, 1)
+				if userHasStrongRsa {
+					atomic.AddUint32(&kt.userWithStrongKeySize, 1)
+				} else {
+					atomic.AddUint32(&kt.userWithWeakKeySize, 1)
+					kt.userWithWeakKey = append(kt.userWithWeakKey, m)
+				}
 			}
 
 			if len(m.Keys) == 0 {
@@ -171,8 +222,14 @@ func generateKeyTable(ms []member) keyTable {
 		}()
 	}
 	wg.Wait()
-	
+
 	return kt
+}
+
+func isRsaStrong(key string) bool {
+	r := regexp.MustCompile(`(ssh-rsa) (.*)`)
+	keyArray := r.FindStringSubmatch(key)
+	return len(keyArray[2]) >= 372
 }
 
 func getKeys(ms []member) []member {
@@ -261,4 +318,8 @@ func getMembers() []member {
 	}
 
 	return members
+}
+
+func main() {
+	printReport(generateKeyTable(getKeys(getMembers())))
 }
